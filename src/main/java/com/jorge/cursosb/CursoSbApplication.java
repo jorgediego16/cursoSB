611edfbb05013d40200297020ebb2691ed16639f
Jorge Diego <jorgediego633sp@gmail.com>
2019-02-06 14:39:47 -0300

    Commit inicial

---
+++ src/main/java/com/jorge/cursosb/CursoSbApplication.java
@@ -0,0 +1,14 @@
+package com.jorge.cursosb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSbApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/jorge/cursosb/CursoSbApplicationTests.java
@@ -0,0 +1,17 @@
+package com.jorge.cursosb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CursoSbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
