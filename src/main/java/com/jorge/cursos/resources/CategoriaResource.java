50de6ebef2de7aec330489c4f1e4d0ba5599845c
Jorge Diego <jorgediego633sp@gmail.com>
2019-02-11 16:06:18 -0300

    Delete categoria

--- src/main/java/com/jorge/cursos/resources/CategoriaResource.java
+++ src/main/java/com/jorge/cursos/resources/CategoriaResource.java
@@ -41,4 +41,10 @@ public class CategoriaResource {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> remove (@RequestBody Categoria obj, @PathVariable Integer id){
+		service.remove(id);
+		return ResponseEntity.noContent().build();
+	}
+	
 }
--- src/main/java/com/jorge/cursos/services/CategoriaService.java
+++ src/main/java/com/jorge/cursos/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.jorge.cursos.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.jorge.cursos.domain.Categoria;
 import com.jorge.cursos.repositories.CategoriaRepository;
+import com.jorge.cursos.services.exception.DataIntegrityException;
 import com.jorge.cursos.services.exception.ObjectNotFoundException;
 
 @Service
@@ -29,5 +31,14 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		buscar(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void remove (Integer id) {
+		buscar(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que tem produtos.");
+		}
 	}
 }
